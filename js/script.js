53415f765a38fca6c1bf88b822b10959a21eedec
Hera <mostafizhera@gmail.com>
2024-09-23 03:17:49 +0600

    noakhali donate  validation successfull

--- js/script.js
+++ js/script.js
@@ -2,12 +2,27 @@ document.getElementById("donateNoakhali").addEventListener("click", function(){
     const myBlance = DonateText("myBlance");
     const noakhaliDonateBlance = DonateText("noakhaliDonateBlance");
     const donateInputMoney = DonateValue("donateInputMoney");
+      
+
+    if(isNaN(donateInputMoney) || donateInputMoney <=0 || donateInputMoney > myBlance){
+        alert("Please enter a valid number");
+        return;
+    }
+
+    const p = document.createElement("div");
+    p.innerHTML = `
+      <div class = "border-2 border-gray-200 p-5 rounded-lg my-3 container mx-auto bg-white">
+        <h3 class = "text-xl font-bold">${donateInputMoney} Taka is Donate for Flood at Noakhali, Bangladesh</h3>
+        <p class = "font-bold">Date: ${new Date()}</p>
+      </div>
+    `;
+   const donateHistory = document.getElementById("donateHistory").appendChild(p);
+   console.log(donateHistory);
+   
 
     const totalNoakhaliDonateBlance = myBlance - donateInputMoney;
 
     document.getElementById("noakhaliDonateBlance").innerText = noakhaliDonateBlance + donateInputMoney;
     document.getElementById("myBlance").innerText = totalNoakhaliDonateBlance;
-    
-    
-    
+        
 })
